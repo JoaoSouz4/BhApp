fb12e110c33fd8420d4c883afaa91d297132ff07
Joao Souza <jvcsouza12@gmail.com>
2022-11-18 05:03:58 -0300

    progresso no desenvolvimento

---
+++ classePrincipal.js
@@ -0,0 +1,10 @@
+class Mercadoria{
+    constructor(nome, tipo, marca, precoCusto, precoVenda){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.marca = marca;
+        this.precoCusto = precoCusto;
+        this.precoVenda = precoVenda;
+        this.status = false;
+    }
+}
--- main.js
+++ main.js
@@ -1,9 +1,43 @@
 
-const bottomRegister = document.querySelector('.bootomS');
+const bottomRegister = document.querySelector('.btn');
+const arrayProductRegister = [];
 
 bottomRegister.addEventListener('click', ()=>{
-    let objectProduct = {
-        
+    
+    const inputNome = document.querySelector('#nome');
+    const inputTipo = document.querySelector('#tipo');
+    const inputQtd = document.querySelector('#qtd');
+    const inputMarca = document.querySelector('#marca');
+    const inputPrecoCusto = document.querySelector('#pc');
+    const inputPrecoVenda = document.querySelector('#pv');
+
+    const nome = inputNome.value;
+    const tipo = inputTipo.value;
+    const qtd = inputQtd.value;
+    const marca = inputMarca.value;
+    const precoCusto = inputPrecoCusto.value;
+    const precoVenda = inputPrecoVenda.value;
+
+    if(nome == ''){
+        alert('Informe um nome válido');
+        return
+    } 
+
+    if(qtd <= 0){
+        alert('Não é possivel cadastrar mercadorias sem quantidade');
+        return;
     }
-})
-const arrayProductRegister = [];
+
+    if(precoCusto == '' || precoVenda == ''){
+        alert('Informe os dados referente aos preços');
+        return
+    }
+
+    alert(`${nome} cadastrado com sucesso`)
+
+    const objeto = new Mercadoria(nome, tipo, qtd, marca, precoCusto, precoVenda);
+    arrayProductRegister.push(objeto);
+
+    console.log(arrayProductRegister);
+});
+
